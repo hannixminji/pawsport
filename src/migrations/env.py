c8cbf2a80f3bddf5b98a936d5ed3ee7c61d8dac9
hannixminji <seon.ahyeon17@gmail.com>
2026-03-12 14:06:28 +0800

    deploy-1

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -34,13 +34,11 @@ def include_object(object, name, type_, reflected, compare_to):
     return True
 
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
 config.set_main_option(
     "sqlalchemy.url",
-    f"{settings.POSTGRES_ASYNC_PREFIX}{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_SERVER}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}",
+    f"{settings.POSTGRES_ASYNC_PREFIX}{settings.POSTGRES_URI}",
 )
 
 if config.config_file_name is not None:
@@ -58,13 +56,6 @@ target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL and not an Engine, though an Engine is acceptable here as well.  By
-    skipping the Engine creation we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the script output.
-    """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
@@ -96,8 +87,6 @@ def do_run_migrations(connection: Connection) -> None:
 
 
 async def run_async_migrations() -> None:
-    """In this scenario we need to create an Engine and associate a connection with the context."""
-
     connectable = async_engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
@@ -111,8 +100,6 @@ async def run_async_migrations() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode."""
-
     asyncio.run(run_async_migrations())
 
 
